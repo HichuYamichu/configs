362740b30b0936bece25a9e269c2dc29bc09155e
HichuYamichu <hichuyamichu@gmail.com>
2023-12-19 08:35:35 +0100

    vim

--- windows/nvim/colors/froob.vim
+++ windows/nvim/colors/froob.vim
@@ -6,76 +6,73 @@ endif
 
 let g:colors_name="Froob"
 
-hi Normal ctermbg=None
-hi Comment    ctermfg=60
-hi Constant   ctermfg=254
-hi Identifier ctermfg=254
-hi Statement  ctermfg=254
-hi Type       ctermfg=254
-hi Special    ctermfg=117
+hi Normal     ctermbg=None guibg=None
+hi Comment    ctermfg=60 guifg=#5f5f87 guibg=None
+hi Constant   ctermfg=254 guifg=#e4e4e4 guibg=None
+hi Identifier ctermfg=254 guifg=#e4e4e4 guibg=None
+hi Statement  ctermfg=254 guifg=#e4e4e4 guibg=None
+hi Type       ctermfg=254 guifg=#e4e4e4 guibg=None
+hi Special    ctermfg=117 guifg=#87d7ff guibg=None
 
-hi WarningMsg ctermfg=231 ctermbg=none cterm=undercurl guisp=#ff5f87
-hi ErrorMsg   ctermfg=Red ctermbg=none
+hi WarningMsg ctermfg=231 ctermbg=none cterm=undercurl guisp=#ff5f87 guifg=#ff5f87 guibg=None
+hi ErrorMsg   ctermfg=160 ctermbg=none guifg=#d70000 guibg=None
 
-hi String         ctermfg=85
-hi Character      ctermfg=86
-hi Number         ctermfg=209
-hi Boolean        ctermfg=228
-hi Float          ctermfg=209
-hi Function       ctermfg=68
-hi Repeat         ctermfg=177
-hi Conditional    ctermfg=177
-hi Label          ctermfg=117
-hi Operator       ctermfg=254
-hi Keyword        ctermfg=177
-hi Include        ctermfg=177
-hi Define         ctermfg=177
-hi Macro          ctermfg=68
-hi Typedef        ctermfg=254
-hi Delimiter      ctermfg=117
-hi SpecialComment ctermfg=60
+hi String         ctermfg=85 guifg=#5fffaf guibg=None
+hi Character      ctermfg=86 guifg=#5fffd7 guibg=None
+hi Number         ctermfg=209 guifg=#ff875f guibg=None
+hi Boolean        ctermfg=228 guifg=#ffff87 guibg=None
+hi Float          ctermfg=209 guifg=#ff875f guibg=None
+hi Function       ctermfg=68 guifg=#5f87d7 guibg=None
+hi Repeat         ctermfg=177 guifg=#d787ff guibg=None
+hi Conditional    ctermfg=177 guifg=#d787ff guibg=None
+hi Label          ctermfg=117 guifg=#87d7ff guibg=None
+hi Operator       ctermfg=254 guifg=#e4e4e4 guibg=None
+hi Keyword        ctermfg=177 guifg=#d787ff guibg=None
+hi Include        ctermfg=177 guifg=#d787ff guibg=None
+hi Define         ctermfg=177 guifg=#d787ff guibg=None
+hi Macro          ctermfg=68 guifg=#5f87d7 guibg=None
+hi Typedef        ctermfg=254 guifg=#e4e4e4 guibg=None
+hi Delimiter      ctermfg=117 guifg=#87d7ff guibg=None
+hi SpecialComment ctermfg=60 guifg=#5f5f87 guibg=None
 
-hi ColorColumn  ctermbg=236
-hi LineNr       ctermfg=243
-hi LineNrAbove  ctermfg=243
-hi LineNrBelow  ctermfg=243
-hi CursorLineNr ctermfg=254 cterm=none
-hi CursorLine ctermbg=233 cterm=none
-hi Cursor       ctermbg=161
+hi ColorColumn  ctermbg=236 guibg=#303030
+hi LineNr       ctermfg=243 guifg=#767676
+hi LineNrAbove  ctermfg=243 guifg=#767676
+hi LineNrBelow  ctermfg=243 guifg=#767676
+hi CursorLineNr ctermfg=254 cterm=none guifg=#e4e4e4 guibg=None
+hi CursorLine ctermbg=233 cterm=none guibg=#121212
+hi Cursor       ctermbg=161 guibg=#d7005f
+hi IncSearch    ctermbg=35 guibg=#00af5f
 
-hi Menu       ctermfg=254
-hi WildMenu   ctermfg=254 ctermbg=236 cterm=none
-hi Pmenu      ctermfg=254 ctermbg=233 
-hi PmenuSel   ctermfg=254 ctermbg=240
-hi PmenuSbar  ctermfg=254 ctermbg=241
-hi PmenuThumb ctermfg=254 ctermbg=236
-hi VertSplit  ctermfg=0   ctermbg=0
-hi MatchParen ctermfg=0   ctermbg=red
+hi Menu       ctermfg=254 guifg=#e4e4e4
+hi WildMenu   ctermfg=254 ctermbg=236 cterm=none guifg=#e4e4e4 guibg=#363636
+hi Pmenu      ctermfg=254 ctermbg=233 guifg=#e4e4e4 guibg=#121212
+hi PmenuSel   ctermfg=254 ctermbg=240 guifg=#e4e4e4 guibg=#585858
+hi PmenuSbar  ctermfg=254 ctermbg=241 guifg=#e4e4e4 guibg=#626262
+hi PmenuThumb ctermfg=254 ctermbg=236 guifg=#e4e4e4 guibg=#303030
+hi MatchParen ctermfg=15 ctermbg=147 guifg=#ffffff guibg=#afafff
 
-hi Folded     ctermfg=248 ctermbg=none cterm=bold
-hi FoldColumn ctermfg=248 ctermbg=none cterm=bold
-hi SignColumn ctermbg=none
+hi Folded     ctermfg=248 ctermbg=none cterm=bold guifg=#a8a8a8 guibg=None gui=bold
+hi FoldColumn ctermfg=248 ctermbg=none cterm=bold guifg=#a8a8a8 guibg=None gui=bold
+hi SignColumn ctermbg=none guibg=None
 
-hi NvimInternalError ctermfg=254
+hi DiagnosticError         ctermfg=204 guifg=#ff5f87
+hi DiagnosticFloatingError ctermfg=204 guifg=#ff5f87
+hi DiagnosticWarn          ctermfg=215 guifg=#ffaf5f
+hi DiagnosticFloatingWarn  ctermfg=215 guifg=#ffaf5f
 
-hi rustStorage       ctermfg=177
-hi rustLifetime      ctermfg=254
-hi rustSigil         ctermfg=117
-hi rustFoldBraces    ctermfg=117
-hi rustModPath       ctermfg=254
-hi rustModPathSep    ctermfg=254
-hi rustDerive        ctermfg=254
-hi rustAttribute     ctermfg=254
-hi rustUnsafeKeyword ctermfg=177
-hi rustSelf			 ctermfg=213
+hi NvimInternalError ctermfg=204 guifg=#ff5f87
 
-hi haskellOperators  ctermfg=117
-hi haskellWhere      ctermfg=177
+hi rustStorage       ctermfg=177 guifg=#d787ff
+hi rustLifetime      ctermfg=254 guifg=#e4e4e4
+hi rustSigil         ctermfg=117 guifg=#87d7ff
+hi rustFoldBraces    ctermfg=117 guifg=#87d7ff
+hi rustModPath       ctermfg=254 guifg=#e4e4e4
+hi rustModPathSep    ctermfg=254 guifg=#e4e4e4
+hi rustDerive        ctermfg=254 guifg=#e4e4e4
+hi rustAttribute     ctermfg=254 guifg=#e4e4e4
+hi rustUnsafeKeyword ctermfg=177 guifg=#d787ff
+hi rustSelf          ctermfg=213 guifg=#ff87ff
 
-hi LspDiagnosticsDefaultError ctermfg=red
-hi LspDiagnosticsUnderlineError cterm=none ctermfg=none
-hi LspDiagnosticsUnderlineHint cterm=none ctermfg=none
-hi LspDiagnosticsUnderlineWarning cterm=none ctermfg=none
-hi LspDiagnosticsUnderlineInformation cterm=none ctermfg=none
-hi LspDIagnosticsDefaultWarning ctermfg=208
-hi LspDIagnosticsDefaultHint ctermfg=60
+hi haskellOperators  ctermfg=117 guifg=#87d7ff
+hi haskellWhere      ctermfg=177 guifg=#d787ff
--- windows/nvim/init.lua
+++ windows/nvim/init.lua
@@ -1,20 +1,9 @@
-local cmd = vim.cmd
-local exec = vim.api.nvim_exec
-local g = vim.g
-local opt = vim.opt
-
-local map = vim.api.nvim_set_keymap
-local default_opts = {
-    noremap = true,
-    silent = true
-}
-
-opt.shell = "pwsh"
-opt.shellxquote = ""
-opt.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command '
-opt.shellquote = ''
-opt.shellpipe = '| Out-File -Encoding UTF8 %s'
-opt.shellredir = '| Out-File -Encoding UTF8 %s'
+vim.opt.shell = "pwsh"
+vim.opt.shellxquote = ""
+vim.opt.shellcmdflag = '-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command '
+vim.opt.shellquote = ''
+vim.opt.shellpipe = '| Out-File -Encoding UTF8 %s'
+vim.opt.shellredir = '| Out-File -Encoding UTF8 %s'
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -48,25 +37,27 @@ require("lazy").setup({
     'rust-lang/rust.vim',
     'L3MON4D3/LuaSnip',
     'saadparwaiz1/cmp_luasnip',
+    'tpope/vim-commentary',
 })
 
-cmd([[set fillchars+=vert:\ ]])
-cmd([[autocmd FileType netrw autocmd BufLeave <buffer> if &filetype == 'netrw' | :bd | endif]])
-cmd([[filetype plugin indent on]])
-cmd('colorscheme froob')
-cmd([[au BufEnter * set fo-=c fo-=r fo-=o]])
-cmd(
+vim.cmd([[set fillchars+=vert:\ ]])
+vim.cmd([[set wrap smoothscroll ]])
+-- vim.cmd([[autocmd FileType netrw autocmd BufLeave <buffer> if &filetype == 'netrw' | :bd | endif]])
+vim.cmd([[autocmd FileType netrw setl bufhidden=wipe]])
+vim.cmd([[let g:netrw_fastbrowse = 0]])
+vim.cmd([[filetype plugin indent on]])
+vim.cmd([[colorscheme froob]])
+vim.cmd([[set termguicolors ]])
+vim.cmd([[au BufEnter * set fo-=c fo-=r fo-=o]])
+vim.cmd(
     [[au BufReadPost * if expand('%:p') !~# '\m/\.git/' && line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]])
 
-g.fzf_vim = {}
-g.fzf_vim.preview_bash = [[C:\Program Files\Git\git-bash.exe]]
-
-cmd([[
+vim.cmd([[
 command! -bang -nargs=? -complete=dir Files
 \ call fzf#vim#files(<q-args>, {'source': 'rg --files --hidden',
-\                               'options': '--tiebreak=index --preview "bat {}" --bind up:preview-up,down:preview-down'}, <bang>0)]])
+\                               'options': ['--tiebreak=index', '--preview', 'bat --color=always --style=numbers {}']}, <bang>0)]])
 
-exec([[
+vim.api.nvim_exec([[
 augroup YankHighlight
   autocmd!
   autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=300}
@@ -133,55 +124,28 @@ cmp.setup({
         }),
     },
     sources = cmp.config.sources({
-        { name = "nvim_lsp", group_index = 2 },
-        { name = "luasnip",  group_index = 2 },
-        { name = "buffer",   group_index = 2 },
-        { name = "nvim_lua", group_index = 2 },
-        { name = "path",     group_index = 2 },
+        { name = "nvim_lsp", },
+        { name = "luasnip",  },
+    },{
+        { name = "buffer",   },
     }),
-    experimental = {
-        ghost_text = true
-    }
 })
 
-cmp.setup.cmdline('/', {
-    sources = { {
-        name = 'buffer'
-    } }
+cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
 })
 
 cmp.setup.cmdline(':', {
-    sources = cmp.config.sources({ {
-        name = 'path'
-    } }, { {
-        name = 'cmdline'
-    } })
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
 })
-
-local on_attach = function(client, bufnr)
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
-    local function buf_set_option(...)
-        vim.api.nvim_buf_set_option(bufnr, ...)
-    end
-
-    local opts = {
-        noremap = true,
-        silent = true
-    }
-    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration({reuse_win = true})<CR>', opts)
-    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition({reuse_win = true})<CR>', opts)
-    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation({reuse_win = true})<CR>', opts)
-    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-    buf_set_keymap('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-    buf_set_keymap('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-    buf_set_keymap('n', '<leader>f', '<cmd>lua vim.lsp.buf.format()<CR>', opts)
-    buf_set_keymap('n', '<leader>g', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-    buf_set_keymap('n', '<leader>i', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-    buf_set_keymap('n', '<C-n>', '<cmd>lua vim.diagnostic.disable()<CR>', opts)
-    buf_set_keymap('n', '<C-m>', '<cmd>lua vim.diagnostic.enable()<CR>', opts)
-end
 
 vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("UserLspConfig", {}),
@@ -190,17 +154,45 @@ vim.api.nvim_create_autocmd("LspAttach", {
         if client.server_capabilities.inlayHintProvider then
             vim.lsp.inlay_hint.enable(args.buf, false)
         end
+        
+        local opts = { buffer = args.buf }
+        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration({reuse_win = true})<CR>', opts)
+        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition({reuse_win = true})<CR>', opts)
+        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation({reuse_win = true})<CR>', opts)
+        vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+        vim.keymap.set('n', '<leader>f', '<cmd>lua vim.lsp.buf.format({async = true})<CR>', opts)
+        vim.keymap.set('n', '<C-i>', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+        vim.keymap.set('n', '<C-m>', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+        vim.keymap.set('n', '<C-n>', '<cmd>lua vim.diagnostic.hover()<CR>', opts)
     end
 })
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = false,
+        signs = {
+            severity = {
+                vim.diagnostic.severity.WARN,
+                vim.diagnostic.severity.ERROR,
+            }
+        },
+        underline = {
+            severity = {
+                vim.diagnostic.severity.ERROR,
+            }
+        },
+        update_in_insert = false
+    }
+)
 
 vim.diagnostic.config({ float = { border = "rounded" } })
 
 require('lspconfig').tsserver.setup {
-    on_attach = on_attach
+}
+
+require('lspconfig').gopls.setup {
 }
 
 require('lspconfig').rust_analyzer.setup {
-    on_attach = on_attach,
     flags = {
         debounce_text_changes = 150
     },
@@ -225,66 +217,58 @@ require('lspconfig').rust_analyzer.setup {
     }
 }
 
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = {
-        severity = 'Error'
-    },
-    signs = {
-        severity = 'Warn'
-    },
-    underline = {
-        severity = 'Warn'
-    },
-    update_in_insert = false
-})
-
 vim.api.nvim_set_hl(0, '@lsp.type.macro.rust', {})
 
 vim.o.completeopt = 'menu,menuone,noselect'
-g.mapleader = " "
-g.nofoldenable = true
-g.noshowmode = true
-g.nojoinspaces = true
+vim.g.mapleader = " "
+vim.g.nofoldenable = true
+vim.g.noshowmode = true
+vim.g.nojoinspaces = true
 
-opt.number = true
-opt.autoindent = true
-opt.timeoutlen = 300
-opt.encoding = 'utf-8'
-opt.scrolloff = 10
-opt.hidden = true
--- opt.printencoding = 'utf-8'
-opt.signcolumn = 'yes'
-opt.clipboard = 'unnamedplus'
-opt.cursorline = true
+vim.opt.number = true
+vim.opt.autoindent = true
+vim.opt.timeoutlen = 300
+vim.opt.encoding = 'utf-8'
+vim.opt.scrolloff = 10
+vim.opt.hidden = true
+vim.opt.signcolumn = 'yes'
+vim.opt.clipboard = 'unnamedplus'
+vim.opt.cursorline = true
 
-opt.splitright = true
-opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
 
-opt.undodir = '/home/hy/.vimdid'
-opt.undofile = true
+vim.opt.undodir = '/home/hy/.vimdid'
+vim.opt.undofile = true
 
-opt.wildmenu = true
-opt.wildmode = 'list:longest'
-opt.wildignore =
+vim.opt.wildmenu = true
+vim.opt.wildmode = 'list:longest'
+vim.opt.wildignore =
 '.hg,.svn,*~,*.png,*.jpg,*.gif,*.settings,Thumbs.db,*.min.js,*.swp,publish/*,intermediate/*,*.o,*.hi,Zend,vendor'
 
-opt.shiftwidth = 4
-opt.softtabstop = 4
-opt.tabstop = 4
-opt.expandtab = true
+vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
 
-opt.incsearch = true
-opt.ignorecase = true
-opt.smartcase = true
-opt.gdefault = true
+vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.gdefault = true
 
-opt.cmdheight = 1
-opt.updatetime = 300
+vim.opt.cmdheight = 1
+vim.opt.updatetime = 300
 
-opt.number = true
-opt.colorcolumn = '100'
-opt.showcmd = true
-opt.mouse = 'a'
+vim.opt.number = true
+vim.opt.colorcolumn = '100'
+vim.opt.showcmd = true
+vim.opt.mouse = 'a'
+
+local map = vim.api.nvim_set_keymap
+local default_opts = {
+    noremap = true,
+    silent = true
+}
 
 _G.inlay_hints_enabled = false
 function ToggleInlayHints()
@@ -293,7 +277,17 @@ function ToggleInlayHints()
 end
 
 map('n', '<leader>o', ':lua ToggleInlayHints()<CR>', default_opts)
-map('n', '<leader>p', '<C-w>', { noremap = true })
+map('n', '<leader>w', ':w<CR>', default_opts)
+map('n', '<leader>q', ':q<CR>', default_opts)
+map('n', '<leader>a', ':bd<CR>', default_opts)
+map('n', '<leader>s', ':Rg<CR>', { noremap = true })
+map('n', '<leader>e', ':e <C-R>=expand("%:p:h") . "\" <CR>', { noremap = true })
+map('n', '<leader><leader>', '<c-^>', { noremap = true })
+
+map('n', '<leader>j', ':wincmd j<CR>', { noremap = true })
+map('n', '<leader>h', ':wincmd h<CR>', { noremap = true })
+map('n', '<leader>k', ':wincmd k<CR>', { noremap = true })
+map('n', '<leader>l', ':wincmd l<CR>', { noremap = true })
 
 map('i', ',', ',<C-g>u', default_opts)
 map('i', '.', '.<C-g>u', default_opts)
@@ -325,15 +319,16 @@ map('l', '<C-k>', '<Esc>', { noremap = true })
 map('t', '<C-k>', '<C-\\><C-n>', { noremap = true })
 
 map('v', '<C-c>', 'y', { noremap = true })
-
-map('n', '<leader>w', ':w<CR>', default_opts)
-map('n', '<leader>q', ':q<CR>', default_opts)
-map('n', '<leader>a', ':bd<CR>', default_opts)
-
 map('n', '<q>', '<C-v', default_opts)
 
 map('', '<C-h>', '0', default_opts)
 map('', '<C-l>', '$', default_opts)
+
+map('', '<C-p>', ':Files<CR>', { noremap = true })
+map('', '<C-o>', ':Buffers<CR>', { noremap = true })
+
+map('n', '<C-a>', ':%', { noremap = true })
+map('v', '<C-_>', '<Plug>Commentary', { noremap = true })
 
 map('v', 'H', 'b', { noremap = true })
 map('n', 'H', 'b', { noremap = true })
@@ -354,16 +349,4 @@ map('x', '<', '<gv', { noremap = true })
 map('n', '<left>', ':bp<CR>', { noremap = true })
 map('n', '<right>', ':bn<CR>', { noremap = true })
 
-map('', '<C-p>', ':Files<CR>', { noremap = true })
-map('', '<C-o>', ':Buffers<CR>', { noremap = true })
 
-map('n', '<leader>s', ':Rg<CR>', { noremap = true })
-map('n', '<leader>e', ':e <C-R>=expand("%:p:h") . "\" <CR>', { noremap = true })
-map('n', '<leader><leader>', '<c-^>', { noremap = true })
-
-map('n', '<leader>j', ':wincmd j<CR>', { noremap = true })
-map('n', '<leader>h', ':wincmd h<CR>', { noremap = true })
-map('n', '<leader>k', ':wincmd k<CR>', { noremap = true })
-map('n', '<leader>l', ':wincmd l<CR>', { noremap = true })
-
-map('n', '<C-a>', ':%', { noremap = true })
